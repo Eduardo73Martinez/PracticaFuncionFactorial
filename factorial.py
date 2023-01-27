0f897e4a22e1e7c1caf20895bf9e29bfa9ca73f1
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2023-01-27 15:28:40 -0300

    correccion de un error relacionado al factorial de cero

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 
 def factorialDe_(numero):
-    """Retorna el factorial del numero dado. En caso de ser menor que 1 da error.
+    """Retorna el factorial del numero dado. En caso de ser menor que 0 da error.
 
     Args:
         numero (int): es el numero que deceamos retornal el factorial
@@ -9,16 +9,16 @@ def factorialDe_(numero):
         int: un número, el resultado de hacer el factorial de #numero
 
     Requirement:
-        El parametro otorgado debe ser mayor o igual a 1.
+        El parametro otorgado debe ser mayor o igual a 0.
     """
-    if (numero>1):
+    if (numero>=0):
         return factorialDe_Osino1(numero)
     else:
-        raise Exception("No se puede es Menor a 1 ")
+        raise Exception("No se puede es Menor a 0 ")
 
 
 def factorialDe_Osino1(numeroFactorial):
-    """retorna el factorial del numero dado. Si es menor a 1 retorna 1.
+    """retorna el factorial del numero dado. Si es menor a 0 retorna 1.
 
     Args:
         numeroFactorial (int): es el numero que deceamos retornal el factorial
@@ -30,11 +30,11 @@ def factorialDe_Osino1(numeroFactorial):
     """
     numeroActual = numeroFactorial
     factorial = 1
-    while ( numeroActual > 1 ):
+    while ( numeroActual > 0 ):
         factorial = factorial * numeroActual
         numeroActual -= 1
 
     return factorial
 
 
-print(factorialDe_(50))
+print(factorialDe_(0))
